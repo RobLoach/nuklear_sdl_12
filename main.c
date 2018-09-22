9d01ca37c231681d6735fd7c4b2dc2a404d23a62
Rob Loach <robloach@gmail.com>
2018-09-22 19:20:31 -0400

    Update the demo

--- main.c
+++ main.c
@@ -19,8 +19,8 @@
 #include "nuklear/nuklear.h"
 #include "nuklear_sdl_12.h"
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
 
 /* ===============================================================
  *
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
     }
 
     ctx = nk_sdl_init(screen_surface);
-    background = nk_rgb(28,48,62);
+    background = nk_rgb(28, 48, 62);
 
     /* style.c */
     /*set_style(ctx, THEME_WHITE);*/
@@ -81,46 +81,50 @@ int main(int argc, char **argv)
         nk_input_end(ctx);
 
         /* GUI */
-        {struct nk_panel layout;
-        if (nk_begin(ctx, "Demo", nk_rect(50, 50, 210, 250),
-            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-            NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
         {
-            enum {EASY, HARD};
-            static int op = EASY;
-            static int property = 20;
-            static char buffer[64];
-            static int len;
-            struct nk_vec2 vecSize;
+            struct nk_panel layout;
+            if (nk_begin(ctx, "Demo", nk_rect(50, 50, 210, 250),
+                NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+                NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+            {
+                enum {EASY, HARD};
+                static int op = EASY;
+                static int property = 20;
+                static char buffer[64];
+                static int len;
+                struct nk_vec2 vecSize;
 
-            nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "button"))
-                fprintf(stdout, "button pressed\n");
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-            nk_layout_row_dynamic(ctx, 25, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-            nk_edit_string(ctx, NK_EDIT_SIMPLE, buffer, &len, 64, 0);
+                nk_layout_row_static(ctx, 30, 80, 1);
+                if (nk_button_label(ctx, "button"))
+                    fprintf(stdout, "button pressed\n");
+                nk_layout_row_dynamic(ctx, 30, 2);
+                if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+                if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+                nk_layout_row_dynamic(ctx, 22, 1);
+                nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+                /* nk_edit_string(ctx, NK_EDIT_SIMPLE, buffer, &len, 64, 0); */
 
-            {struct nk_panel combo;
-            nk_layout_row_dynamic(ctx, 30, 1);
-            nk_label(ctx, "background:", NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            vecSize.x = 400;
-            vecSize.y = 10;
-            if (nk_combo_begin_color(ctx, background, vecSize)) {
-                nk_layout_row_dynamic(ctx, 120, 1);
-                newBackground = nk_color_picker(ctx, nk_color_cf(background), NK_RGBA);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                background.r = (nk_byte)nk_propertyi(ctx, "#R:", 0, newBackground.r * 255.0f, 255, 1,1);
-                background.g = (nk_byte)nk_propertyi(ctx, "#G:", 0, newBackground.g * 255.0f, 255, 1,1);
-                background.b = (nk_byte)nk_propertyi(ctx, "#B:", 0, newBackground.b * 255.0f, 255, 1,1);
-                background.a = (nk_byte)nk_propertyi(ctx, "#A:", 0, newBackground.a * 255.0f, 255, 1,1);
-                nk_combo_end(ctx);
-            }}
+                {
+                    struct nk_panel combo;
+                    nk_layout_row_dynamic(ctx, 20, 1);
+                    nk_label(ctx, "background:", NK_TEXT_LEFT);
+                    nk_layout_row_dynamic(ctx, 25, 1);
+                    vecSize.x = nk_widget_width(ctx);
+                    vecSize.y = 400;
+                    if (nk_combo_begin_color(ctx, background, vecSize)) {
+                        nk_layout_row_dynamic(ctx, 120, 1);
+                        newBackground = nk_color_picker(ctx, nk_color_cf(background), NK_RGBA);
+                        nk_layout_row_dynamic(ctx, 25, 1);
+                        background.r = (nk_byte)nk_propertyi(ctx, "#R:", 0, newBackground.r * 255.0f, 255, 1,1);
+                        background.g = (nk_byte)nk_propertyi(ctx, "#G:", 0, newBackground.g * 255.0f, 255, 1,1);
+                        background.b = (nk_byte)nk_propertyi(ctx, "#B:", 0, newBackground.b * 255.0f, 255, 1,1);
+                        background.a = (nk_byte)nk_propertyi(ctx, "#A:", 0, newBackground.a * 255.0f, 255, 1,1);
+                        nk_combo_end(ctx);
+                    }
+                }
+            }
+            nk_end(ctx);
         }
-        nk_end(ctx);}
 
         /* -------------- EXAMPLES ---------------- */
         /*calculator(ctx);*/
@@ -130,7 +134,7 @@ int main(int argc, char **argv)
 
         /* Draw */
         nk_color_fv(bg, background);
-        nk_sdl_render(nk_rgb(30,30,30));
+        nk_sdl_render(nk_rgb(background.r, background.g, background.b));
     }
 
 cleanup:
